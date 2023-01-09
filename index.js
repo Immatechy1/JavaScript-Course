b3b0df96624886f30c09705e3c8ba95f478eb01b
Nwokejeme <immaculetanwokejeme@gmail.com>
2023-01-09 04:00:49 +0100

    GLOBAL AND LOCAL VARIABLE

--- index.js
+++ index.js
@@ -99,11 +99,27 @@
 
 // getAverage (7, 12);
 
-function getAverage (a,b) {
-    var average = (a + b) /2
-    console.log(average)
-    return average;
-}
+// function getAverage (a,b) {
+//     var average = (a + b) /2
+//     console.log(average)
+//     return average;
+// }
 
-var myResult = getAverage (7, 12);
-console.log("the average is" + myResult);
+// var myResult = getAverage (7, 12);
+// console.log("the average is" + myResult);
+
+// GLOBAL AND LOCAL VARIABLE
+// function getAverage (a,b) {
+//     var average = (a + b) /2; //local variable
+//     console.log(average)
+//     return average;
+// }
+
+// var myResult = getAverage (7, 12); // global variable
+
+// function logResult() {
+//     console.log("the average is" + myResult + "inside the function");
+// }
+
+// logResult();
+
